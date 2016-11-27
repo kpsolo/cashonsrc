3fc8216bcf61bf3b9e59bfac204f02487270dbd9
aleksey.severnyuk <aleksey.severnyuk@uvoteam.com>
2016-11-27 22:55:43 +0200

    fix home page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,19 @@
-(function ($, window, document, undefined) {
+$(function() {
 
-  'use strict';
+    var header = $(".home-scroll");
+    if ($(window).scrollTop() > 0) {
+        header.addClass("home-scroll-active");
+    }
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
 
-  $(function () {
-    // FastShell
-  });
+        if (scroll > 0) {
+            header.addClass("home-scroll-active");
+        } else {
+            header.removeClass("home-scroll-active");
+        }
+    });
 
-})(jQuery, window, document);
+});
+
+
