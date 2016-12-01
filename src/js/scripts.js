1b0e5253b000baa5609082cb7f3d8b8e745441c9
Aleksey Severnyuk <aleksey.severnyuk@uvoteam.com>
2016-12-01 16:07:20 +0200

    fix css

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -89,15 +89,18 @@ $('ul.event-list').each(function() {
 });
 
 $(document).ready(function(){
-  $(".submenu a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
+
+$('.submenu a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 800);
+            return false;
+        }
     }
-  });
+});
+
 });
