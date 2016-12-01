eb17eea212e1b4acb16aa6cfb5c48aa90e114b44
NickolasZ <nickolasz@gmail.com>
2016-12-01 10:28:50 +0200

    add smooth scroll

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -73,3 +73,17 @@ $('ul.event-list').each(function() {
         e.preventDefault();
     });
 });
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
